cd8f8ddc56db8f02b9f2c3bf6f0a79bc86a72c53
Karlos Kliemann <karloskliemann@gmail.com>
2022-11-13 14:50:42 -0300

    copying files to git

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './styles.css';
+import { Card } from '../../components/Card';
+
+export function Home() {
+  const [studentName, setStudentName] = useState("");
+
+  return (
+    <div className='container'>
+      <h1>Nome: {studentName}</h1>
+      <input 
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+
+      <button type="button">Adicionar</button>
+
+    <Card name="Karlos" time="11:55:00"/>
+    <Card name="João" time="11:56:55"/>
+    <Card name="Ana" time="10:30:39"/>
+    <Card />
+
+    </div>
+  )
+}
